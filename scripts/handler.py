72a081b64c929ca627f2262bec52af3d9ef0fb52
Andrew Carter <acarter@cs.hmc.edu>
2012-08-30 11:46:25 -0700

    Template for python handler of files

--- scripts/handler.py
+++ scripts/handler.py
@@ -24,3 +24,8 @@
 # The views and conclusions contained in the software and documentation are those
 # of the authors and should not be interpreted as representing official policies, 
 # either expressed or implied, of the FreeBSD Project.
+from cgitb import enable
+enable()
+
+from .scripts.webapp import run
+run()
